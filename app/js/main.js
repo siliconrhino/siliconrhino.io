d82ec0e6cfdb69a96f17fa2d3db6920dd89205f6
wodlander <wodlander@gmail.com>
2014-12-03 21:26:19 +0000

    Twitter feed

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('#tweecool').tweecool({
+	username : 'siliconrhino',
+	limit : 1, // Number of tweets to show
+	profile_image : false, // Show profile image
+	show_time : false, // Show tweet time
+	show_media : false,
+	show_media_size: 'thumb'  //values: small, large, thumb, medium 
+	});
+});
