576b76821989a12d97a6ebcec4ce78fae582ec42
wodlander <wodlander@gmail.com>
2015-11-27 17:31:43 +0000

    menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,20 @@
 (function ($) {
 	$(document).ready(function() {
-	$('#tweecool').tweecool({
-	username : 'siliconrhino',
-	limit : 1, // Number of tweets to show
-	profile_image : false, // Show profile image
-	show_time : false, // Show tweet time
-	show_media : false,
-	show_media_size: 'thumb'  //values: small, large, thumb, medium
+		$('#tweecool').tweecool({
+			username : 'siliconrhino',
+			limit : 1, // Number of tweets to show
+			profile_image : false, // Show profile image
+			show_time : false, // Show tweet time
+			show_media : false,
+			show_media_size: 'thumb'  //values: small, large, thumb, medium
+		});
+	
+		$('.modalbutton').click(function() {
+			$('.modal').modal('hide');
+		});
+		$("#menu").click(function(){
+			$(".menu > .right").toggleClass("show");
+			$(".header").toggleClass("blue");
+		});
 	});
-
-   $('.modalbutton').click(function() {
-    $('.modal').modal('hide');
-});
-});
 })(window.$);
