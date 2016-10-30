897868080bcfe4081fcabc86b8f6d5d0bcfcde15
Phil Hardy <phil@siliconrhino.io>
2016-10-30 13:42:25 +0000

    Replace javascript animations with css animations

--- app/js/main_scroll.js
+++ app/js/main_scroll.js
@@ -71,7 +71,6 @@ jQuery(document).ready(function ($) {
         if (MQ === 'desktop' && bool) {
             //bind the animation to the window scroll event, arrows click and keyboard
             if (hijacking === 'on') {
-                initHijacking();
                 var lastSuccessfulScrollTime,
                   scrollMaxThrottle = 1000,
                   scrollMinThrottle = 50,
@@ -128,61 +127,6 @@ jQuery(document).ready(function ($) {
         }
     }
 
-    function scrollAnimation() {
-        //normal scroll - use requestAnimationFrame (if defined) to optimize performance
-        (!window.requestAnimationFrame) ? animateSection(): window.requestAnimationFrame(animateSection);
-    }
-
-    function animateSection() {
-        var scrollTop = $(window).scrollTop(),
-            windowHeight = $(window).height(),
-            windowWidth = $(window).width();
-
-        sectionsAvailable.each(function () {
-            var actualBlock = $(this),
-                offset = scrollTop - actualBlock.offset().top;
-
-            //according to animation type and window scroll, define animation parameters
-            var animationValues = setSectionAnimation(offset, windowHeight, animationType);
-
-            transformSection(actualBlock.children('div'),  animationValues[0],animationValues[1], animationValues[2], animationValues[3], animationValues[4], animationValues[5]);
-            (offset >= 0 && offset < windowHeight) ? actualBlock.addClass('visible'): actualBlock.removeClass('visible');
-        });
-
-        checkNavigation();
-    }
-
-    function transformSection(element, translateY, scaleValue, rotateXValue, opacityValue, boxShadow) {
-        //transform sections - normal scroll
-        element.velocity({
-            translateY: translateY + 'vh',
-            scale: scaleValue,
-            rotateX: rotateXValue,
-            opacity: opacityValue,
-            boxShadowBlur: boxShadow + 'px',
-            translateZ: 0,
-        }, 0);
-    }
-
-    function initHijacking() {
-        // initialize section style - scrollhijacking
-        var visibleSection = sectionsAvailable.filter('.visible'),
-            topSection = visibleSection.prevAll('.cd-section'),
-            bottomSection = visibleSection.nextAll('.cd-section'),
-            animationParams = selectAnimation(animationType, false),
-            animationVisible = animationParams[0],
-            animationTop = animationParams[1],
-            animationBottom = animationParams[2];
-
-        visibleSection.children('div').velocity(animationVisible, 1, function () {
-            visibleSection.css('opacity', 1);
-            topSection.css('opacity', 1);
-            bottomSection.css('opacity', 1);
-        });
-        topSection.children('div').velocity(animationTop, 0);
-        bottomSection.children('div').velocity(animationBottom, 0);
-    }
-
     function scrollHijacking(event) {
         // on mouse scroll - check if animate section
         if (event.originalEvent.detail < 0 || event.originalEvent.wheelDelta > 0) {
@@ -203,13 +147,10 @@ jQuery(document).ready(function ($) {
             middleScroll = (hijacking === 'off' && $(window).scrollTop() !== visibleSection.offset().top) ? true : false;
         visibleSection = middleScroll ? visibleSection.next('.cd-section') : visibleSection;
 
-        var animationParams = selectAnimation(animationType, middleScroll, 'prev');
-        unbindScroll(visibleSection.prev('.cd-section'), animationParams[3]);
-
         if (!animating && !visibleSection.is(':first-of-type')) {
             animating = true;
 
-            transitionBetweenSections(visibleSection, visibleSection.prev('.cd-section'), false, animationParams);
+            transitionBetweenSections(visibleSection, visibleSection.prev('.cd-section'), false);
 
             var goto =
                 visibleSection.prev('.cd-section')[0].dataset.section;
@@ -220,10 +161,8 @@ jQuery(document).ready(function ($) {
            var newCurrentSection = sectionsAvailable.filter('.visible');
             shuttleAnimation(newCurrentSection);
 
-
             actual = actual - 1;
         }
-
 
         resetScroll();
     }
@@ -235,13 +174,10 @@ jQuery(document).ready(function ($) {
         var visibleSection = sectionsAvailable.filter('.visible'),
             middleScroll = (hijacking === 'off' && $(window).scrollTop() !== visibleSection.offset().top) ? true : false;
 
-        var animationParams = selectAnimation(animationType, middleScroll, 'next');
-        unbindScroll(visibleSection.next('.cd-section'), animationParams[3]);
-
         if (!animating && !visibleSection.is(":last-of-type")) {
             animating = true;
 
-            transitionBetweenSections(visibleSection, visibleSection.next('.cd-section'), true, animationParams);
+            transitionBetweenSections(visibleSection, visibleSection.next('.cd-section'), true);
 
             var goto =
                 visibleSection.next('.cd-section')[0].dataset.section;
@@ -267,12 +203,9 @@ jQuery(document).ready(function ($) {
             middleScroll = (hijacking == 'off' && $(window).scrollTop() != visibleSection.offset().top) ? true : false;
         var goToSection = $(".cd-section[data-section='" + goTo + "']");
 
-        var animationParams = selectAnimation(animationType, middleScroll, 'next');
-        unbindScroll(goToSection, animationParams[3]);
-
         if (!animating) {
             animating = true;
-            transitionBetweenSections(visibleSection, goToSection, true, animationParams);
+            transitionBetweenSections(visibleSection, goToSection, true);
         }
         resetScroll();
     }
@@ -293,18 +226,6 @@ jQuery(document).ready(function ($) {
 
 
     }
-
-
-    function unbindScroll(section, time) {
-        //if clicking on navigation - unbind scroll and animate using custom velocity animation
-        if (hijacking == 'off') {
-            $(window).off('scroll', scrollAnimation);
-            (animationType == 'catch') ? $('body, html').scrollTop(section.offset().top): section.velocity("scroll", {
-                duration: time
-            });
-        }
-    }
-
 
     function resetScroll() {
         delta = 0;
@@ -329,181 +250,27 @@ jQuery(document).ready(function ($) {
         return window.getComputedStyle(document.querySelector('body'), '::before').getPropertyValue('content').replace(/"/g, "").replace(/'/g, "");
     }
 
-    function selectAnimation(animationName, middleScroll, direction) {
-        // select section animation - scrollhijacking
-        var animationVisible = 'translateNone',
-            animationTop = 'translateUp',
-            animationBottom = 'translateDown',
-            easing = 'ease',
-            animDuration = 800;
+    function transitionBetweenSections(oldCurrent, newCurrent, translateUp) {
+      var newPrevious = newCurrent.prev('.cd-section');
+      var newNext = newCurrent.next('.cd-section');
 
-        switch (animationName) {
-        case 'opacity':
-            animDuration = 700;
-            animationTop = 'hide.scaleUp';
-            animationBottom = 'hide.scaleDown';
-            break;
-        case 'parallax':
-            animationTop = 'translateUp.half';
-            easing = 'easeInCubic';
-            break;
-        }
+      oldCurrent.removeClass('visible');
 
-        return [animationVisible, animationTop, animationBottom, animDuration, easing];
+      if (oldCurrent[0] === newPrevious[0] || oldCurrent[0] === newNext[0]) {
+        newPrevious.removeClass('up').addClass('down');
+        newNext.removeClass('down').addClass('up');
+      } else {
+        newPrevious.removeClass('up').addClass('down');
+        newNext.removeClass('down').addClass('up');
+        oldCurrent.removeClass('up').addClass('down');
+      }
+
+      newCurrent.addClass('visible');
+
+      setTimeout(function() {
+        animating = false;
+      }, 700);
     }
-
-    function transitionBetweenSections(previous, next, translateUp, animationParams) {
-      previous
-        .removeClass('visible')
-        .children('div')
-          .velocity(translateUp ? animationParams[1] : animationParams[2], animationParams[3], animationParams[4]);
-
-      next
-        .addClass('visible')
-        .children('div')
-          .velocity(animationParams[0], animationParams[3], animationParams[4], function () {
-            animating = false;
-            if (hijacking == 'off') $(window).on('scroll', scrollAnimation);
-          });
-    }
-
-    function setSectionAnimation(sectionOffset, windowHeight, animationName) {
-        // select section animation - normal scroll
-        var scale = 1,
-            translateY = 100,
-            rotateX = '0deg',
-            opacity = 1,
-            boxShadowBlur = 0;
-
-        if (sectionOffset >= -windowHeight && sectionOffset <= 0) {
-            // section entering the viewport
-            translateY = (-sectionOffset) * 100 / windowHeight;
-
-            switch (animationName) {
-            case 'opacity':
-                translateY = 0;
-                scale = (sectionOffset + 7 * windowHeight) * 0.2 / windowHeight;
-                opacity = (sectionOffset + windowHeight) / windowHeight;
-                break;
-            }
-
-        } else if (sectionOffset > 0 && sectionOffset <= windowHeight) {
-            //section leaving the viewport - still has the '.visible' class
-            translateY = (-sectionOffset) * 100 / windowHeight;
-
-            switch (animationName) {
-            case 'opacity':
-                translateY = 0;
-                scale = (sectionOffset + 7 * windowHeight) * 0.2 / windowHeight;
-                opacity = (windowHeight - sectionOffset) / windowHeight;
-                break;
-            case 'parallax':
-                translateY = (-sectionOffset) * 50 / windowHeight;
-                break;
-
-            }
-
-        } else if (sectionOffset < -windowHeight) {
-            //section not yet visible
-            translateY = 100;
-
-            switch (animationName) {
-            case 'opacity':
-                translateY = 0;
-                scale = 0.8;
-                opacity = 0;
-                break;
-            }
-
-        } else {
-            //section not visible anymore
-            translateY = -100;
-
-            switch (animationName) {
-            case 'opacity':
-                translateY = 0;
-                scale = 1.2;
-                opacity = 0;
-                break;
-            case 'parallax':
-                translateY = -50;
-                break;
-            }
-        }
-
-        return [translateY, scale, rotateX, opacity, boxShadowBlur];
-    }
-
-
-});
-
-/* Custom effects registration - feature available in the Velocity UI pack */
-//none
-$.Velocity
-    .RegisterEffect("translateUp", {
-        defaultDuration: 1,
-        calls: [
-            [{
-                translateY: '-100%'
-            }, 1]
-        ]
-    });
-$.Velocity
-    .RegisterEffect("translateDown", {
-        defaultDuration: 1,
-        calls: [
-            [{
-                translateY: '100%'
-            }, 1]
-        ]
-    });
-$.Velocity
-    .RegisterEffect("translateNone", {
-        defaultDuration: 1,
-        calls: [
-            [{
-                translateY: '0',
-                opacity: '1',
-                scale: '1',
-                rotateX: '0',
-                boxShadowBlur: '0'
-            }, 1]
-        ]
-    });
-
-
-//opacity
-$.Velocity
-    .RegisterEffect("hide.scaleUp", {
-        defaultDuration: 1,
-        calls: [
-            [{
-                opacity: '0',
-                scale: '1.2'
-            }, 1]
-        ]
-    });
-$.Velocity
-    .RegisterEffect("hide.scaleDown", {
-        defaultDuration: 1,
-        calls: [
-            [{
-                opacity: '0',
-                scale: '0.8'
-            }, 1]
-        ]
-    });
-//parallax
-$.Velocity
-    .RegisterEffect("translateUp.half", {
-        defaultDuration: 1,
-        calls: [
-            [{
-                translateY: '-50%'
-            }, 1]
-        ]
-    });
-
 
 // Vertical menu
 
@@ -525,16 +292,13 @@ $.Velocity
 
  // var sectionToAnimateOn = document.getElementById('section5');
 
-      // function shuttleAnimation(current)
-      // {
-         // if (sectionToAnimateOn === current[0])
-         //{
-             // current[0].className = "space-shuttle-pic rhino";
-        // }
+      function shuttleAnimation(current)
+      {
 
-      //}
+      }
 
 
+});
 
 
 
