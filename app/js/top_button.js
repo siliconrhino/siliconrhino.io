f212655ca2a042a6f56f4f76dc900a45b2029209
Jamie <jamie.olivier@siliconrhino.io>
2020-08-05 11:45:50 +0100

    safari scroll fix

--- app/js/top_button.js
+++ app/js/top_button.js
@@ -8,12 +8,46 @@ function showbtn() {
   }
 }
 
-function myFunction() {
-  window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+function scrollFunction() {
+    if ('scrollBehavior' in document.documentElement.style) { //Checks if browser supports scroll function
+        window.scroll({
+            top : 0,
+            left : 0,
+            behavior : 'smooth'
+        });
+    } else {
+        smoothScrollTo(0, 0, 1000);
+    }
 }
+
+
+window.smoothScrollTo = function(endX, endY, duration) {
+        let startX = window.scrollX || window.pageXOffset,
+        startY = window.scrollY || window.pageYOffset,
+        distanceX = endX - startX,
+        distanceY = endY - startY,
+        startTime = new Date().getTime();
+
+        // Easing function
+        let easeInOutQuart = function(time, from, distance, duration) {
+            if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+            return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+        };
+
+        let timer = window.setInterval(function() {
+            let time = new Date().getTime() - startTime,
+            newX = easeInOutQuart(time, startX, distanceX, duration),
+            newY = easeInOutQuart(time, startY, distanceY, duration);
+            if (time >= duration) {
+                window.clearInterval(timer);
+            }
+            window.scrollTo(newX, newY);
+        }, 1000 / 60); // 60 fps
+    };
+
 if (btn) {
   btn.onclick = function () {
-    myFunction();
+   scrollFunction();
   };
 }
 window.addEventListener('scroll', showbtn);
