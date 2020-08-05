e82f758e5ba81f46b1d5983d7f2163e667e96a3d
Jamie <jamie.olivier@siliconrhino.io>
2020-08-05 09:48:25 +0100

    fixing singlequotes

--- app/js/top_button.js
+++ app/js/top_button.js
@@ -1,19 +1,19 @@
-var btn = document.getElementById("toTopButton");
+var btn = document.getElementById('toTopButton');
 
 function showbtn() {
   if (window.scrollY > 300) {
-    btn.classList.add("show");
+    btn.classList.add('show');
   } else {
-    btn.classList.remove("show");
+    btn.classList.remove('show');
   }
 }
 
 function myFunction() {
-  window.scroll({ top: 0, left: 0, behavior: "smooth" });
+  window.scroll({ top: 0, left: 0, behavior: 'smooth' });
 }
 if (btn) {
   btn.onclick = function () {
     myFunction();
   };
 }
-window.addEventListener("scroll", showbtn);
+window.addEventListener('scroll', showbtn);
