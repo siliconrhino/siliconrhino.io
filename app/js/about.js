f5fd50b0ea6e9c53323913452a7518d06b40c637
Jamie <jamie.olivier@siliconrhino.io>
2020-08-12 14:03:29 +0100

    Working on the links

--- app/js/about.js
+++ app/js/about.js
@@ -12,5 +12,20 @@ if (previous) {
     carouselScroll.scrollBy(-280, 0);
   })
 }
+function findPos(obj) {
+  var curtop = 0;
+  if (obj.offsetParent) {
+    do {
+      curtop += obj.offsetTop;
+    } while (obj = obj.offsetParent);
+    return [curtop];
+  }
+}
+var phil = document.getElementById("phil").offsetTop;
+function scrollToTeam() {
+  console.log(phil);
 
+  window.scrollTo({ top: phil - 70, behavior: 'smooth' });
+  ;
+}
 
