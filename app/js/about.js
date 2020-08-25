02468e3f42ae1ac2459e1f1934d0ea7f3cfe5d6b
Roger Planes <roger@siliconrhino.io>
2020-08-25 12:55:58 +0200

    event preventdefault

--- app/js/about.js
+++ app/js/about.js
@@ -3,52 +3,58 @@ var previous = document.getElementById("previous");
 var carouselScroll = document.getElementById("carousel");
 
 if (next) {
-  next.addEventListener('click', () => {
+  next.addEventListener("click", () => {
     carouselScroll.scrollBy(280, 0);
-  })
+  });
 }
 if (previous) {
-  previous.addEventListener('click', () => {
+  previous.addEventListener("click", () => {
     carouselScroll.scrollBy(-280, 0);
-  })
+  });
 }
 function findPos(obj) {
   var curtop = 0;
   if (obj.offsetParent) {
     do {
       curtop += obj.offsetTop;
-    } while (obj = obj.offsetParent);
+    } while ((obj = obj.offsetParent));
     return [curtop];
   }
 }
 var carousel_wrap = document.getElementById("team-carousel");
 carousel_wrap.addEventListener("click", function (event) {
   const person = event.target;
-  var minimised = carousel_wrap.classList.contains('minimize')
+  var minimised = carousel_wrap.classList.contains("minimize");
   console.log(person.dataset.name);
 
-  var personpos = document.querySelector('#' + person.dataset.name);
-  const rect = personpos.getBoundingClientRect() // get rects(width, height, top, etc)
-  const viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  var personpos = document.querySelector("#" + person.dataset.name);
+  const rect = personpos.getBoundingClientRect(); // get rects(width, height, top, etc)
+  const viewHeight = Math.max(
+    document.documentElement.clientHeight,
+    window.innerHeight || 0
+  );
 
-  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+  event.preventDefault();
+
+  if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
     console.log("in mobile");
-    window.scroll({
-      top: rect.top + rect.height / 2 - viewHeight / 2
-
+    const scrollTop = rect.top + rect.height / 2 - viewHeight / 2;
+    window.scrollTo({
+      top: scrollTop,
     });
   } else if (minimised) {
     console.log("minimized");
-    window.scroll({
-      top: rect.top + rect.height / 2 - viewHeight / 2
-
+    window.scrollTo({
+      top: rect.top + rect.height / 2 - viewHeight / 2,
     });
   } else {
     console.log("normal");
-    window.scroll({
-      top: rect.top + rect.height / 2 - viewHeight / 2 - 450
-
+    window.scrollTo({
+      top: rect.top + rect.height / 2 - viewHeight / 2 - 450,
     });
   }
 });
-
