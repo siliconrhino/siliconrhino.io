ad4120679a9ea2254f83b11fed161e782eeeb4bc
Jamie <jamie.olivier@siliconrhino.io>
2020-09-09 17:26:07 +0100

    (no branch):

--- app/js/top_button.js
+++ app/js/top_button.js
@@ -7,28 +7,28 @@ function showbtn() {
     btn.classList.remove('show');
   }
 }
-function smoothScrollTo(endX, endY, duration){
-let startX = window.scrollX || window.pageXOffset,
-        startY = window.scrollY || window.pageYOffset,
-        distanceX = endX - startX,
-        distanceY = endY - startY,
-        startTime = new Date().getTime();
+function smoothScrollTo(endX, endY, duration) {
+  var startX = window.scrollX || window.pageXOffset,
+    startY = window.scrollY || window.pageYOffset,
+    distanceX = endX - startX,
+    distanceY = endY - startY,
+    startTime = new Date().getTime();
 
-        // Easing function
-        let easeInOutQuart = function(time, from, distance, duration) {
-            if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
-            return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
-        };
+  // Easing function
+  var easeInOutQuart = function (time, from, distance, duration) {
+    if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+    return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+  };
 
-        let timer = window.setInterval(function() {
-            let time = new Date().getTime() - startTime,
-            newX = easeInOutQuart(time, startX, distanceX, duration),
-            newY = easeInOutQuart(time, startY, distanceY, duration);
-            if (time >= duration) {
-                window.clearInterval(timer);
-            }
-            window.scrollTo(newX, newY);
-        }, 1000 / 60);
+  var timer = window.setInterval(function () {
+    var time = new Date().getTime() - startTime,
+      newX = easeInOutQuart(time, startX, distanceX, duration),
+      newY = easeInOutQuart(time, startY, distanceY, duration);
+    if (time >= duration) {
+      window.clearInterval(timer);
+    }
+    window.scrollTo(newX, newY);
+  }, 1000 / 60);
 }
 
 function scrollFunction() {
