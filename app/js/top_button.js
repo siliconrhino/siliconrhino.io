9cf5f26bf728cf21af148affb62c60ff5dc13d38
Jamie <jamie.olivier@siliconrhino.io>
2020-09-10 16:30:15 +0100

    build error fix

--- app/js/top_button.js
+++ app/js/top_button.js
@@ -7,45 +7,45 @@ function showbtn() {
     btn.classList.remove('show');
   }
 }
-function smoothScrollTo(endX, endY, duration){
-let startX = window.scrollX || window.pageXOffset,
-        startY = window.scrollY || window.pageYOffset,
-        distanceX = endX - startX,
-        distanceY = endY - startY,
-        startTime = new Date().getTime();
+function smoothScrollTo(endX, endY, duration) {
+  var startX = window.scrollX || window.pageXOffset,
+    startY = window.scrollY || window.pageYOffset,
+    distanceX = endX - startX,
+    distanceY = endY - startY,
+    startTime = new Date().getTime();
 
-        // Easing function
-        let easeInOutQuart = function(time, from, distance, duration) {
-            if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
-            return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
-        };
+  // Easing function
+  var easeInOutQuart = function (time, from, distance, duration) {
+    if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+    return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+  };
 
-        let timer = window.setInterval(function() {
-            let time = new Date().getTime() - startTime,
-            newX = easeInOutQuart(time, startX, distanceX, duration),
-            newY = easeInOutQuart(time, startY, distanceY, duration);
-            if (time >= duration) {
-                window.clearInterval(timer);
-            }
-            window.scrollTo(newX, newY);
-        }, 1000 / 60);
+  var timer = window.setInterval(function () {
+    var time = new Date().getTime() - startTime,
+      newX = easeInOutQuart(time, startX, distanceX, duration),
+      newY = easeInOutQuart(time, startY, distanceY, duration);
+    if (time >= duration) {
+      window.clearInterval(timer);
+    }
+    window.scrollTo(newX, newY);
+  }, 1000 / 60);
 }
 
 function scrollFunction() {
-    if ('scrollBehavior' in document.documentElement.style) { //Checks if browser supports scroll function
-        window.scroll({
-            top : 0,
-            left : 0,
-            behavior : 'smooth'
-        });
-    } else {
-        smoothScrollTo(0, 0, 1000);
-    }
+  if ('scrollBehavior' in document.documentElement.style) { //Checks if browser supports scroll function
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  } else {
+    smoothScrollTo(0, 0, 1000);
+  }
 }
 
 if (btn) {
   btn.onclick = function () {
-   scrollFunction();
+    scrollFunction();
   };
 }
 window.addEventListener('scroll', showbtn);
